ca40fcdf42e2fcc29de3b7f73c6ee4f6a129029a
yrjwcharm <xlyanrui@sina.com>
2019-07-26 15:30:52 +0800

    更新 内容

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export  default class App extends Component<Props> {
 
   componentDidMount() {
       let fontName = 'PingFang-SC-Regular'
-      GlobalFont.applyGlobal(fontName)
+       GlobalFont.applyGlobal(fontName)
   }
 
   render() {
